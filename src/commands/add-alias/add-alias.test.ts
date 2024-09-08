c850e8b22f1926f24965121c1db09cf0b7654366
Milton Carlos Katoo <mckatoo@gmail.com>
2024-09-08 19:34:23 -0300

    test errors

--- src/commands/add-alias/add-alias.test.ts
+++ src/commands/add-alias/add-alias.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, spyOn, mock, test, jest } from "bun:test";
+import { describe, expect, spyOn, mock, test, jest, afterAll, beforeAll } from "bun:test";
 import { Command } from "commander";
 import addAlias from ".";
 import fs from "fs";
@@ -6,53 +6,60 @@ import child_process from "child_process"
 
 
 describe("Add alias", () => {
-    test("when arguments includes -- then stop processing options", () => {
+    beforeAll(() => {
         const readFileSyncSpy = spyOn(fs, 'readFileSync')
         readFileSyncSpy.mockImplementation(jest.fn().mockReturnValue(''))
         const appendFileSyncSpy = spyOn(fs, 'appendFileSync')
         appendFileSyncSpy.mockImplementation(jest.fn().mockReturnValue(null))
         const execSyncSpy = spyOn(child_process, 'execSync')
         execSyncSpy.mockImplementation(jest.fn().mockReturnValue(null))
+    })
 
+    afterAll(() => {
+        mock.restore()
+    })
+
+    test("When arguments includes -- then stop processing options", () => {
         const app = new Command("aliases");
+        app.exitOverride()
         addAlias(app)
-        app.parse(["node", "aliases", "add", "-n l", "-c ls", "--", "--type", "order-cake"]);
+        app.parse(["node", "aliases", "add", "-n l", "-c ls", "--", "--type", "list"]);
         const opts = app.commands[0].opts()
         const args = app.commands[0].args
         const name = opts.name.replace(" ", "")
 
         expect(name).toBe("l");
         expect(opts.type).toBeUndefined();
-        expect(args).toEqual(["--type", "order-cake"]);
+        expect(args).toEqual(["--type", "list"]);
     });
 
-    // test("unknown option, then handle error", () => {
-    //     const program = new Command();
-    //     program
-    //         .exitOverride()
-    //         .command("order-cake")
-    //         .action(() => { });
-    //     let caughtErr: any;
-    //     try {
-    //         program.parse(["node", "palatial-cakes-cli", "order-cake", "--color"]);
-    //     } catch (err) {
-    //         caughtErr = err;
-    //     }
-    //     expect(caughtErr.code).toBe("commander.unknownOption");
-    // });
+    test("Unknown option, then handle error", () => {
+        const program = new Command();
+        program.exitOverride()
+        program
+            .exitOverride()
+            .command("add")
+            .action(() => { });
 
-    // test("unknown command, then handle error", () => {
-    //     const program = new Command();
-    //     program
-    //         .exitOverride()
-    //         .command("order-cake")
-    //         .action(() => { });
-    //     let caughtErr: any;
-    //     try {
-    //         program.parse(["node", "palatial-cakes-cli", "make-order"]);
-    //     } catch (err) {
-    //         caughtErr = err;
-    //     }
-    //     expect(caughtErr.code).toBe("commander.unknownCommand");
-    // });
+        let caughtErr: any;
+        try {
+            program.parse(["node", "aliases", "add", "--color"]);
+        } catch (err) {
+            caughtErr = err;
+        }
+        expect(caughtErr.code).toBe("commander.unknownOption");
+    });
+
+    test("Unknown command, then handle error", () => {
+        const program = new Command();
+        program.exitOverride()
+        addAlias(program)
+        let caughtErr: any;
+        try {
+            program.parse(["node", "aliases", "list"]);
+        } catch (err) {
+            caughtErr = err;
+        }
+        expect(caughtErr.code).toBe("commander.unknownCommand");
+    });
 })
