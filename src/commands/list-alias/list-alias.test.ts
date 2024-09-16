00f42c2f091f3c28554f3ba5b5d620d89ec3a989
Milton Carlos Katoo <mckatoo@gmail.com>
2024-09-16 18:04:14 -0300

    test success on list alias

--- src/commands/list-alias/list-alias.test.ts
+++ src/commands/list-alias/list-alias.test.ts
@@ -1,27 +1,16 @@
 import { afterEach, beforeEach, describe, expect, jest, mock, spyOn, test } from "bun:test";
-import child_process from "child_process";
 import { Command } from "commander";
 import fs from "fs";
-import readLine from "readline-sync";
 import { ALIASES_PATH } from "src/utils/envs";
 import listAlias from ".";
 
 
 let readFileSyncSpy: any
-let appendFileSyncSpy: any
-let execSyncSpy: any
-let keyInYNSpy: any
 
 describe("List aliases", () => {
     beforeEach(() => {
         readFileSyncSpy = spyOn(fs, 'readFileSync')
-        appendFileSyncSpy = spyOn(fs, 'appendFileSync')
-        execSyncSpy = spyOn(child_process, 'execSync')
-        keyInYNSpy = spyOn(readLine, 'keyInYN')
-
         readFileSyncSpy.mockImplementation(jest.fn().mockReturnValue(''))
-        appendFileSyncSpy.mockImplementation(jest.fn())
-        execSyncSpy.mockImplementation(jest.fn())
     })
 
     afterEach(() => {
@@ -40,70 +29,44 @@ describe("List aliases", () => {
         expect(args).toEqual(["--type", "rm"]);
     });
 
-    // test("Unknown option, then handle error", () => {
-    //     const program = new Command();
-    //     program.exitOverride()
-    //     program
-    //         .exitOverride()
-    //         .command("add")
-    //         .action(() => { });
+    test("Unknown option, then handle error", () => {
+        const program = new Command();
+        program.exitOverride()
+        listAlias(program)
 
-    //     let caughtErr: any;
-    //     try {
-    //         program.parse(["node", "aliases", "add", "--color"]);
-    //     } catch (err) {
-    //         caughtErr = err;
-    //     }
-    //     expect(caughtErr.code).toBe("commander.unknownOption");
-    // });
+        let caughtErr: any;
+        try {
+            program.parse(["node", "aliases", "list", "--color"]);
+        } catch (err) {
+            caughtErr = err;
+        }
+        expect(caughtErr.code).toBe("commander.unknownOption");
+    });
 
-    // test("Unknown command, then handle error", () => {
-    //     const program = new Command();
-    //     program.exitOverride()
-    //     addAlias(program)
-    //     let caughtErr: any;
-    //     try {
-    //         program.parse(["node", "aliases", "list"]);
-    //     } catch (err) {
-    //         caughtErr = err;
-    //     }
-    //     expect(caughtErr.code).toBe("commander.unknownCommand");
-    // });
+    test("Unknown command, then handle error", () => {
+        const program = new Command();
+        program.exitOverride()
+        listAlias(program)
+        let caughtErr: any;
+        try {
+            program.parse(["node", "aliases", "add"]);
+        } catch (err) {
+            caughtErr = err;
+        }
+        expect(caughtErr.code).toBe("commander.unknownCommand");
+    });
 
-    // test("Success on add alias", () => {
-    //     const program = new Command();
-    //     program.exitOverride()
-    //     addAlias(program)
-    //     program.parse(["node", "aliases", "add", "-n l", "-c exa -l"]);
-    //     const alias = "alias  l=\" exa -l\""
-    //     const opts = program.commands[0].opts()
-    //     const name = opts.name.replace(" ", "")
-    //     const command = opts.command.replace(" ", "")
+    test("Success on list alias", () => {
+        readFileSyncSpy.mockImplementation(jest.fn().mockReturnValue(Buffer.from('alias l="exa -l"')))
+        const consoleLogSpy = spyOn(console, 'log')
+        consoleLogSpy.mockImplementation(jest.fn().mockReturnValue('l="exa -l"'))
+        
+        const program = new Command();
+        program.exitOverride()
+        listAlias(program)
+        program.parse(["node", "aliases", "list"]);
 
-    //     expect(name).toBe('l')
-    //     expect(command).toBe('exa -l')
-    //     expect(appendFileSyncSpy).toHaveBeenLastCalledWith(ALIASES_PATH, alias)
-    //     expect(execSyncSpy).toHaveBeenCalledWith(`sed -i '/^$/d' ${ALIASES_PATH}`)
-    // })
-
-    // test("Success on update alias", () => {
-    //     readFileSyncSpy.mockImplementation(jest.fn().mockReturnValue('alias  l=\" exa -l\"'))
-    //     keyInYNSpy.mockImplementation(jest.fn().mockReturnValue(true))
-
-    //     const program = new Command();
-    //     program.exitOverride()
-    //     addAlias(program)
-    //     program.parse(["node", "aliases", "add", "-n l", "-c exa -l"]);
-
-    //     const alias = 'alias  l=\" exa -l\"'
-    //     const opts = program.commands[0].opts()
-    //     const name = opts.name.replace(" ", "")
-    //     const command = opts.command.replace(" ", "")
-
-    //     expect(name).toBe('l')
-    //     expect(command).toBe('exa -l')
-    //     expect(appendFileSyncSpy).toHaveBeenLastCalledWith(ALIASES_PATH, alias)
-    //     expect(execSyncSpy).toHaveBeenNthCalledWith(1, `sed -i '/^alias  ${name}=/d' ${ALIASES_PATH}`)
-    //     expect(execSyncSpy).toHaveBeenLastCalledWith(`sed -i '/^$/d' ${ALIASES_PATH}`)
-    // })
+        expect(readFileSyncSpy).toBeCalledWith(ALIASES_PATH)
+        expect(consoleLogSpy).toBeCalledWith('l="exa -l"')
+    })
 })
